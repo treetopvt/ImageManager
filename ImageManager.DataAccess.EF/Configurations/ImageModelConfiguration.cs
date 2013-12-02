4346e7cb36332dbb9d2e03bbdfecdc2ca32c2df7
treetopvt <bull.thomas@gmail.com>
2013-12-02 16:25:28 -0500

    Created dataaccess assembly and switched to an EF metadata format that follows the DocCode dto example from Breeze.

---
+++ ImageManager.DataAccess.EF/Configurations/ImageModelConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ImageManager.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageManager.DataAccess.EF.Configurations
+{
+    internal class ImageModelConfiguration : EntityTypeConfiguration<ImageModel>
+    {
+        public ImageModelConfiguration()
+        {
+            //Property(i=>i.Id).IsRequired();
+            Property(i=>i.FileName).IsRequired();
+
+        }
+    }
+}
---
+++ ImageManager.DataAccess.EF/ImageManagerContext.cs
@@ -0,0 +1,32 @@
+﻿using ImageManager.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageManager.DataAccess.EF
+{
+    internal class ImageManagerContext :DbContext
+    {
+            
+        static ImageManagerContext()
+        {
+            // Prevent attempt to initialize a database for this context
+            Database.SetInitializer<ImageManagerContext>(null);
+        }
+ 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new Configurations.ImageModelConfiguration());
+            //modelBuilder.Configurations.Add(new OrderDtoConfiguration());
+            //modelBuilder.Configurations.Add(new OrderDetailDtoConfiguration());
+            //modelBuilder.Configurations.Add(new ProductDtoConfiguration());
+        }
+ 
+        public DbSet<ImageModel> Images { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        //public DbSet<Product> Products { get; set; }
+     }
+}
